71ec67a50153f1c43a3ad5358c322f64cbac5871
neo-dev <sei133dirsoa@gmail.com>
2026-04-05 15:43:53 -0300

    Complete task4 on task list 1

--- task-list1/task4.lua
+++ task-list1/task4.lua
@@ -3,6 +3,25 @@
 
 os.setlocale("pt_BR.UTF-8", "all")
 
-local function check(a, b, c)
-        return a < b + c and b < a + c and c < b + a
+math.randomseed(os.time())
+
+local a = math.random(0, 20)
+local b = math.random(0, 20)
+local c = math.random(0, 20)
+
+local tipo = ""
+
+if not (a < b + c) and not (b < a + c) and not (c < b + a) then
+        print('não se pode fazer um triângulo com', a, b, c)
+        os.exit(0)
 end
+
+if a == b and a == c then
+        tipo = tipo .. "Equilátero"
+elseif a == b or a == c or b == c then -- faltou verificar b == c ;-;
+        tipo = tipo .. "Isósceles"
+else
+        tipo = tipo .. "Escaleno"
+end
+
+print('O tipo de triângulo que ' .. a .. ', ' .. b .. ' e ' .. c .. ' formam é ' .. tipo .. '.')
